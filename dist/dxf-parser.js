3f721c3401af5b65ecf071021b709a46cabfa2fa
Ben Zuill-Smith <bzuillsmith@gmail.com>
2018-01-03 23:18:27 -0800

    Updated dist file

--- dist/dxf-parser.js
+++ dist/dxf-parser.js
@@ -1003,6 +1003,10 @@ DxfParser.prototype._parse = function(dxfString) {
 					log.debug('LINE {');
 					entity = parseLINE();
 					log.debug('}');
+				 } else if (curr.value === '3DFACE') {
+					log.debug('3DFACE {');
+					entity = parse3DFACE();
+					log.debug('}');
 				} else if(curr.value === 'CIRCLE') {
 					log.debug('CIRCLE {');
 					entity = parseCIRCLE();
@@ -1043,6 +1047,10 @@ DxfParser.prototype._parse = function(dxfString) {
 				} else if(curr.value === 'INSERT') {
 					log.debug('INSERT {');
 					entity = parseINSERT();
+					log.debug('}');
+				} else if(curr.value == 'SPLINE') {
+					log.debug('INSERT {');
+					entity = parseSPLINE();
 					log.debug('}');
 				} else {
 					log.warn('Unhandled entity ' + curr.value);
@@ -1259,9 +1267,8 @@ DxfParser.prototype._parse = function(dxfString) {
 						if(curr.value != 0) vertex.bulge = curr.value;
 						break;
 					default:
-						//todo: mark unhandled somehow?
-						curr = scanner.next();
-						continue;
+						// if we do not hit known code return vertices.  Code might belong to entity
+						return vertices;
 				}
 				curr = scanner.next();
 			}
@@ -1445,6 +1452,86 @@ DxfParser.prototype._parse = function(dxfString) {
 
 		return entity;
 	};
+	
+	var parse3dFaceVertices = function(entity) {
+        var vertices = [],
+            i;
+        var vertexIsStarted = false;
+        var vertexIsFinished = false;
+        var verticesPer3dFace = 4; // there can be up to four vertices per face, although 3 is most used for TIN
+		
+        for (i = 0; i <= verticesPer3dFace; i++) {
+            var vertex = {};
+            while (curr !== 'EOF') {
+                if (curr.code === 0 || vertexIsFinished) break;
+
+                switch (curr.code) {
+                    case 10: // X0
+                    case 11: // X1
+                    case 12: // X2
+                    case 13: // X3
+                        if (vertexIsStarted) {
+                            vertexIsFinished = true;
+                            continue;
+                        }
+                        vertex.x = curr.value;
+                        vertexIsStarted = true;
+                        break;
+                    case 20: // Y
+                    case 21:
+                    case 22:
+                    case 23:
+                        vertex.y = curr.value;
+                        break;
+                    case 30: // Z
+                    case 31:
+                    case 32:
+                    case 33:
+                        vertex.z = curr.value;
+                        break;
+                    default:
+                        // it is possible to have entity codes after the vertices.  
+                        // So if code is not accounted for return to entity parser where it might be accounted for
+                        return vertices;
+                        continue;
+                }
+                curr = scanner.next();
+            }
+            // See https://groups.google.com/forum/#!topic/comp.cad.autocad/9gn8s5O_w6E
+            vertices.push(vertex);
+            vertexIsStarted = false;
+            vertexIsFinished = false;
+        }
+        return vertices;
+    };
+
+    /**
+     * Called when the parser reads the beginning of a new entity,
+     * 0:parse3DFACE. Scanner.next() will return the first attribute of the
+     * entity.
+     * @return {Object} the entity parsed
+     */
+    var parse3DFACE = function() {
+        var entity = { type: curr.value, vertices: [] };
+          curr = scanner.next();
+        while (curr !== 'EOF') {
+            if (curr.code === 0) break;
+            switch (curr.code) {
+                case 70: // 1 = Closed shape, 128 = plinegen?, 0 = default
+                    entity.shape = ((curr.value & 1) === 1);
+                    entity.hasContinuousLinetypePattern = ((curr.value & 128) === 128);
+                    curr = scanner.next();
+                    break;
+                case 10: // X coordinate of point
+                    entity.vertices = parse3dFaceVertices();
+                    break;
+                default:
+                    checkCommonEntityProperties(entity);
+                    break;
+            }
+        }
+        return entity;
+    };
 
 	/**
 	 * Called when the parser reads the beginning of a new entity,
@@ -1482,6 +1569,18 @@ DxfParser.prototype._parse = function(dxfString) {
 					break;
 				case 43:
 					if(curr.value !== 0) entity.width = curr.value;
+					curr = scanner.next();
+					break;
+				case 210:
+					entity.extrusionDirectionX = curr.value;
+					curr = scanner.next();
+					break;
+				case 220:
+					entity.extrusionDirectionY = curr.value;
+					curr = scanner.next();
+					break;
+				case 230:
+					entity.extrusionDirectionZ = curr.value;
 					curr = scanner.next();
 					break;
 				default:
@@ -1806,6 +1905,75 @@ DxfParser.prototype._parse = function(dxfString) {
 		return entity;
 	};
 
+	var parseSPLINE = function() {
+		var entity;
+		entity = { type: curr.value };
+		curr = scanner.next();
+		while(curr !== 'EOF')
+		{
+			if(curr.code === 0) break;
+
+			switch(curr.code) {
+				case 10:
+					if (!entity.controlPoints) entity.controlPoints = [];
+					entity.controlPoints.push(parsePoint());
+					break;
+				case 11:
+					if (!entity.fitPoints) entity.fitPoints = [];
+					entity.fitPoints.push(parsePoint());
+					break;
+				case 12:
+					entity.startTangent = parsePoint();
+					break;
+				case 13:
+					entity.endTangent = parsePoint();
+					break;
+				case 40:
+					if (!entity.knotValues) entity.knotValues = [];
+					entity.knotValues.push(curr.value);
+					curr = scanner.next();
+					break;
+				case 70:
+					if ((curr.value & 1) != 0) entity.closed = true;
+					if ((curr.value & 2) != 0) entity.periodic = true;
+					if ((curr.value & 4) != 0) entity.rational = true;
+					if ((curr.value & 8) != 0) entity.planar = true;
+					if ((curr.value & 16) != 0) 
+					{
+						entity.planar = true;
+						entity.linear = true;
+					}
+					curr = scanner.next();
+					break;
+					
+				case 71:
+					entity.degreeOfSplineCurve = curr.value;
+					curr = scanner.next();
+					break;
+				case 72:
+					entity.numberOfKnots = curr.value;
+					curr = scanner.next();
+					break;
+				case 73:
+					entity.numberOfControlPoints = curr.value;
+					curr = scanner.next();
+					break;
+				case 74:
+					entity.numberOfFitPoints = curr.value;
+					curr = scanner.next();
+					break;
+				case 210:
+					entity.normalVector = parsePoint();
+					break;
+				default:
+					checkCommonEntityProperties(entity);
+					break;
+			}
+		}
+
+		return entity;
+	};
+
 	var parseELLIPSE = function() {
 		var entity;
 		entity = { type: curr.value };
@@ -1927,30 +2095,30 @@ module.exports = DxfParser;
 * Licensed under the MIT license.
 */
 (function (root, definition) {
-    if (typeof module === 'object' && module.exports && typeof require === 'function') {
-        module.exports = definition();
-    } else if (typeof define === 'function' && typeof define.amd === 'object') {
+    "use strict";
+    if (typeof define === 'function' && define.amd) {
         define(definition);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = definition();
     } else {
         root.log = definition();
     }
 }(this, function () {
-    var self = {};
+    "use strict";
+
+    // Slightly dubious tricks to cut down minimized file size
     var noop = function() {};
     var undefinedType = "undefined";
 
-    function realMethod(methodName) {
-        if (typeof console === undefinedType) {
-            return false; // We can't build a real method without a console to log to
-        } else if (console[methodName] !== undefined) {
-            return bindMethod(console, methodName);
-        } else if (console.log !== undefined) {
-            return bindMethod(console, 'log');
-        } else {
-            return noop;
-        }
-    }
+    var logMethods = [
+        "trace",
+        "debug",
+        "info",
+        "warn",
+        "error"
+    ];
 
+    // Cross-browser bind equivalent that works at least back to IE6
     function bindMethod(obj, methodName) {
         var method = obj[methodName];
         if (typeof method.bind === 'function') {
@@ -1967,117 +2135,208 @@ module.exports = DxfParser;
         }
     }
 
-    function enableLoggingWhenConsoleArrives(methodName, level) {
+    // Build the best logging method possible for this env
+    // Wherever possible we want to bind, not wrap, to preserve stack traces
+    function realMethod(methodName) {
+        if (methodName === 'debug') {
+            methodName = 'log';
+        }
+
+        if (typeof console === undefinedType) {
+            return false; // No method possible, for now - fixed later by enableLoggingWhenConsoleArrives
+        } else if (console[methodName] !== undefined) {
+            return bindMethod(console, methodName);
+        } else if (console.log !== undefined) {
+            return bindMethod(console, 'log');
+        } else {
+            return noop;
+        }
+    }
+
+    // These private functions always need `this` to be set properly
+
+    function replaceLoggingMethods(level, loggerName) {
+        /*jshint validthis:true */
+        for (var i = 0; i < logMethods.length; i++) {
+            var methodName = logMethods[i];
+            this[methodName] = (i < level) ?
+                noop :
+                this.methodFactory(methodName, level, loggerName);
+        }
+
+        // Define log.log as an alias for log.debug
+        this.log = this.debug;
+    }
+
+    // In old IE versions, the console isn't present until you first open it.
+    // We build realMethod() replacements here that regenerate logging methods
+    function enableLoggingWhenConsoleArrives(methodName, level, loggerName) {
         return function () {
             if (typeof console !== undefinedType) {
-                replaceLoggingMethods(level);
-                self[methodName].apply(self, arguments);
+                replaceLoggingMethods.call(this, level, loggerName);
+                this[methodName].apply(this, arguments);
             }
         };
     }
 
-    var logMethods = [
-        "trace",
-        "debug",
-        "info",
-        "warn",
-        "error"
-    ];
-
-    function replaceLoggingMethods(level) {
-        for (var i = 0; i < logMethods.length; i++) {
-            var methodName = logMethods[i];
-            self[methodName] = (i < level) ? noop : self.methodFactory(methodName, level);
-        }
+    // By default, we use closely bound real methods wherever possible, and
+    // otherwise we wait for a console to appear, and then try again.
+    function defaultMethodFactory(methodName, level, loggerName) {
+        /*jshint validthis:true */
+        return realMethod(methodName) ||
+               enableLoggingWhenConsoleArrives.apply(this, arguments);
     }
 
-    function persistLevelIfPossible(levelNum) {
-        var levelName = (logMethods[levelNum] || 'silent').toUpperCase();
+    function Logger(name, defaultLevel, factory) {
+      var self = this;
+      var currentLevel;
+      var storageKey = "loglevel";
+      if (name) {
+        storageKey += ":" + name;
+      }
 
-        // Use localStorage if available
-        try {
-            window.localStorage['loglevel'] = levelName;
-            return;
-        } catch (ignore) {}
+      function persistLevelIfPossible(levelNum) {
+          var levelName = (logMethods[levelNum] || 'silent').toUpperCase();
 
-        // Use session cookie as fallback
-        try {
-            window.document.cookie = "loglevel=" + levelName + ";";
-        } catch (ignore) {}
-    }
+          if (typeof window === undefinedType) return;
 
-    function loadPersistedLevel() {
-        var storedLevel;
+          // Use localStorage if available
+          try {
+              window.localStorage[storageKey] = levelName;
+              return;
+          } catch (ignore) {}
 
-        try {
-            storedLevel = window.localStorage['loglevel'];
-        } catch (ignore) {}
+          // Use session cookie as fallback
+          try {
+              window.document.cookie =
+                encodeURIComponent(storageKey) + "=" + levelName + ";";
+          } catch (ignore) {}
+      }
 
-        if (typeof storedLevel === undefinedType) {
-            try {
-                storedLevel = /loglevel=([^;]+)/.exec(window.document.cookie)[1];
-            } catch (ignore) {}
-        }
-        
-        if (self.levels[storedLevel] === undefined) {
-            storedLevel = "WARN";
-        }
+      function getPersistedLevel() {
+          var storedLevel;
 
-        self.setLevel(self.levels[storedLevel], false);
+          if (typeof window === undefinedType) return;
+
+          try {
+              storedLevel = window.localStorage[storageKey];
+          } catch (ignore) {}
+
+          // Fallback to cookies if local storage gives us nothing
+          if (typeof storedLevel === undefinedType) {
+              try {
+                  var cookie = window.document.cookie;
+                  var location = cookie.indexOf(
+                      encodeURIComponent(storageKey) + "=");
+                  if (location !== -1) {
+                      storedLevel = /^([^;]+)/.exec(cookie.slice(location))[1];
+                  }
+              } catch (ignore) {}
+          }
+
+          // If the stored level is not valid, treat it as if nothing was stored.
+          if (self.levels[storedLevel] === undefined) {
+              storedLevel = undefined;
+          }
+
+          return storedLevel;
+      }
+
+      /*
+       *
+       * Public logger API - see https://github.com/pimterry/loglevel for details
+       *
+       */
+
+      self.name = name;
+
+      self.levels = { "TRACE": 0, "DEBUG": 1, "INFO": 2, "WARN": 3,
+          "ERROR": 4, "SILENT": 5};
+
+      self.methodFactory = factory || defaultMethodFactory;
+
+      self.getLevel = function () {
+          return currentLevel;
+      };
+
+      self.setLevel = function (level, persist) {
+          if (typeof level === "string" && self.levels[level.toUpperCase()] !== undefined) {
+              level = self.levels[level.toUpperCase()];
+          }
+          if (typeof level === "number" && level >= 0 && level <= self.levels.SILENT) {
+              currentLevel = level;
+              if (persist !== false) {  // defaults to true
+                  persistLevelIfPossible(level);
+              }
+              replaceLoggingMethods.call(self, level, name);
+              if (typeof console === undefinedType && level < self.levels.SILENT) {
+                  return "No console available for logging";
+              }
+          } else {
+              throw "log.setLevel() called with invalid level: " + level;
+          }
+      };
+
+      self.setDefaultLevel = function (level) {
+          if (!getPersistedLevel()) {
+              self.setLevel(level, false);
+          }
+      };
+
+      self.enableAll = function(persist) {
+          self.setLevel(self.levels.TRACE, persist);
+      };
+
+      self.disableAll = function(persist) {
+          self.setLevel(self.levels.SILENT, persist);
+      };
+
+      // Initialize with the right level
+      var initialLevel = getPersistedLevel();
+      if (initialLevel == null) {
+          initialLevel = defaultLevel == null ? "WARN" : defaultLevel;
+      }
+      self.setLevel(initialLevel, false);
     }
 
     /*
      *
-     * Public API
+     * Top-level API
      *
      */
 
-    self.levels = { "TRACE": 0, "DEBUG": 1, "INFO": 2, "WARN": 3,
-        "ERROR": 4, "SILENT": 5};
+    var defaultLogger = new Logger();
 
-    self.methodFactory = function (methodName, level) {
-        return realMethod(methodName) ||
-               enableLoggingWhenConsoleArrives(methodName, level);
-    };
-
-    self.setLevel = function (level, persist) {
-        if (typeof level === "string" && self.levels[level.toUpperCase()] !== undefined) {
-            level = self.levels[level.toUpperCase()];
+    var _loggersByName = {};
+    defaultLogger.getLogger = function getLogger(name) {
+        if (typeof name !== "string" || name === "") {
+          throw new TypeError("You must supply a name when creating a logger.");
         }
-        if (typeof level === "number" && level >= 0 && level <= self.levels.SILENT) {
-            if (persist !== false) {  // defaults to true
-                persistLevelIfPossible(level);
-            }
-            replaceLoggingMethods(level);
-            if (typeof console === undefinedType && level < self.levels.SILENT) {
-                return "No console available for logging";
-            }
-        } else {
-            throw "log.setLevel() called with invalid level: " + level;
+
+        var logger = _loggersByName[name];
+        if (!logger) {
+          logger = _loggersByName[name] = new Logger(
+            name, defaultLogger.getLevel(), defaultLogger.methodFactory);
         }
-    };
-
-    self.enableAll = function(persist) {
-        self.setLevel(self.levels.TRACE, persist);
-    };
-
-    self.disableAll = function(persist) {
-        self.setLevel(self.levels.SILENT, persist);
+        return logger;
     };
 
     // Grab the current global log variable in case of overwrite
     var _log = (typeof window !== undefinedType) ? window.log : undefined;
-    self.noConflict = function() {
+    defaultLogger.noConflict = function() {
         if (typeof window !== undefinedType &&
-               window.log === self) {
+               window.log === defaultLogger) {
             window.log = _log;
         }
 
-        return self;
+        return defaultLogger;
     };
 
-    loadPersistedLevel();
-    return self;
+    defaultLogger.getLoggers = function getLoggers() {
+        return _loggersByName;
+    };
+
+    return defaultLogger;
 }));
 
 },{}]},{},[3])(3)
